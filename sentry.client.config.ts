21559d9f0d892a165fb13c1b1c4dd20140c77c0c
TheTeaCup <lw0482352@gmail.com>
2023-12-29 13:56:10 -0500

    sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: ":DSN@sentry.kastelapp.com/:ID",
+  dsn: "https://3944922fa93e7efba54d001526936864@sentry.kastelapp.com/3",
   ...(process.env.KASTEL_DESKTOP_APP === "false" && {
     tunnel: "/api/monitor",
   }),
